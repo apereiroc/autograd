188c4710b62645175a88003ebc71de1893fec5cc
Marcos Romero Lamas <marromlam@gmail.com>
2026-02-15 17:01:33 +0100

    feat: add backpropagation

--- autodiff.h
+++ autodiff.h
@@ -1,61 +1,41 @@
 #ifndef AUTOGRAD_AUTOGRAD_H
 #define AUTOGRAD_AUTOGRAD_H
 
-#include <memory>
 #include "graph.h"
 #include "variable.h"
+#include <memory>
 
 // TODO: add documentation
 
 class AutoDiff {
-  public:
-    AutoDiff()
-        : g(std::make_unique<Graph>())
-    {}
-    
+public:
+  AutoDiff() : g(std::make_unique<Graph>()) {}
 
-    /* void restart_graph() */
-    /* { */
-    /*   g->restart(); */
-    /*   delete g; */
-    /*   g = new Graph(); */
-    /*   std::cout << "Graph restarted." << std::endl; */
-    /* } */
+  // Old API (returns by value)
+  template <typename T> Variable<T> variable(const T &value) {
+    Variable x(value);
+    g->add_variable(x);
+    return x;
+  }
 
-    template <typename T>
-    Variable<T> variable(const T& value) {
-      Variable x(value);
-      g->add_variable(x);
-      return x;
-    }
+  // New API (returns pointer for backward pass)
+  Variable<double> *variable_ptr(double value) {
+    auto var = std::make_unique<Variable<double>>(value);
+    return g->add_variable_ptr(std::move(var));
+  }
 
-    void print_variables() {
-      g->print_variables_for_debug();
-    }
+  void print_variables() { g->print_variables_for_debug(); }
 
-    void print_operators() {
-      g->print_operators_for_debug();
-    }
+  void print_operators() { g->print_operators_for_debug(); }
+
+  // Run backward pass from a variable
+  void backward(Variable<double> &root_var) { g->backward(root_var); }
+
+  // Zero all gradients
+  void zero_gradients() { g->zero_gradients(); }
 
 private:
-    std::unique_ptr<Graph> g;
+  std::unique_ptr<Graph> g;
 };
 
-
-
-
-
-
-
-// class Autograd{
-//   public:
-//     Autograd();
-//     ~Autograd();
-//     Variable variable();
-//     Variable variable(double y);
-//     void print();
-//   private:
-//     Graph* g;
-// };
-
-#endif //AUTOGRAD_AUTOGRAD_H
+#endif // AUTOGRAD_AUTOGRAD_H
--- variable.h
+++ variable.h
@@ -2,24 +2,27 @@
 #define AUTOGRAD_VARIABLE_H
 
 #include <cstddef>
+#include <iostream>
+#include <stdexcept>
+#include <cmath>
+#include <limits>
 #include "node.h"
-#include "graph.h"
 
-//TODO: add documentation
+// Forward declaration to avoid circular dependency
 class Graph;
 
 template <typename T = double>
 class Variable : public Node {
   public:
-    //Variable() { _value = 0; }
-
     Variable(const T& value)
     : _value(value),
+      _gradient(0),
       _g(nullptr)
     {}
 
     Variable(const T& value, Graph *g)
     : _value(value),
+      _gradient(0),
       _g(g)
     {}
 
@@ -27,70 +30,23 @@ class Variable : public Node {
 
     const T& get_value() const { return _value; }
 
+    void set_gradient(const T& grad) { _gradient = grad; }
+
+    const T& get_gradient() const { return _gradient; }
+
+    void add_gradient(const T& grad) { _gradient += grad; }
+
+    void zero_gradient() { _gradient = 0; }
+
     void set_graph(Graph* g) {_g = g ;}
 
     const Graph* get_graph() const { return _g; }
 
-    Variable operator+(const Variable& other) const
-    {
-        if (this->_g != other.get_graph()){
-            std::cout << "ERROR: variables exist in different graphs" << std::endl;
-            exit(1);
-        }
-
-        Variable result( this->_value + other.get_value(), this->_g);
-        if (this->_g != nullptr)
-          this->_g->add_variable(result);
-
-        return result;
-    }
-
-    Variable operator-(const Variable& other) const
-    {
-      if (this->_g != other.get_graph()){
-          std::cout << "ERROR: variables exist in different graphs" << std::endl;
-          exit(1);
-      }
-
-      Variable result( this->_value - other.get_value(), this->_g);
-      if (this->_g != nullptr)
-        this->_g->add_variable(result);
-
-      return result;
-    }
-
-    Variable operator*(const Variable& other) const
-    {
-      if (this->_g != other.get_graph()){
-          std::cout << "ERROR: variables exist in different graphs" << std::endl;
-          exit(1);
-      }
-
-      Variable result( this->_value * other.get_value(), this->_g);
-
-      if (this->_g != nullptr)
-        this->_g->add_variable(result);
-
-      return result;
-    }
-
-    Variable operator/(const Variable& other) const
-    {
-      if (other.get_value() == 0)
-          throw std::runtime_error("Division by 0");
-      
-      if (this->_g != other.get_graph()){
-          std::cout << "ERROR: variables exist in different graphs" << std::endl;
-          exit(1);
-      }
-
-      Variable result( this->_value / other.get_value() );
-
-      if (this->_g != nullptr)
-        this->_g->add_variable(result);
-      
-      return result;
-    }
+    // Operator declarations - implementations at end of file
+    Variable operator+(const Variable& other) const;
+    Variable operator-(const Variable& other) const;
+    Variable operator*(const Variable& other) const;
+    Variable operator/(const Variable& other) const;
 
     bool operator==(const Variable& other) const
     {
@@ -107,8 +63,6 @@ class Variable : public Node {
         return this->get_value() == other;
     }
 
-    //TODO: implement more operators
-
     friend std::ostream& operator<<(std::ostream& os, const Variable& var){
       os << var.get_value();
       return os;
@@ -116,6 +70,7 @@ class Variable : public Node {
 
   private:
     T _value;
+    T _gradient;
     Graph* _g;
 };
 
